a6182f8b0334f5d8e7f977939c591104803b586a
Fernando Cordero <fernan942@gmail.com>
2019-08-27 21:55:37 -0400

    clase 5, config asistida de rutas con express

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res)=>{
-    res.send("Hola, estoy en la ruta '/'!");
-});
+// app.get("/", (req, res)=>{
+//     res.send("Hola, estoy en la ruta '/'!");
+// });
+//Eliminamos estas lineas porque no es eficiente tener todaslas rutas en el index
+//asi que usamos el otro fichero routes
+require("./routes/views")(app)
+require("./routes/special")(app)
+
 
 console.log("Iniciando Express.js");
 app.listen(3000, ()=>{
