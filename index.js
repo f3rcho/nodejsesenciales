beccef02e506d394e77c93da0e5df3ae139e3189
Fernando Cordero <fernan942@gmail.com>
2019-08-28 14:15:39 -0400

    consumiendo operaciones de un API publico

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const express = require("express");
+const logger = require("morgan")
+const bodyParser = require("body-parser")
+
 const app = express();
+
+app.use( logger("dev") ) // agregamos los middlewares con app.use. los midle siempre nos van a devolver funciones y app.use siempre va a esperar una fun
+app.use( bodyParser.json() ) // para json
+app.use( bodyParser.urlencoded({ extended: false }) ) //para urlencoded, 
+
 
 require("./routes/views")(app);
 require("./routes/special")(app);
